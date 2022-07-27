87e0761f1e25560b44d59d19b51300b6d0c5c83f
Afkonfire <110054232+Afkonfire@users.noreply.github.com>
2022-07-27 21:00:03 +0530

    Update Ahh.py

--- handlers/Ahh.py
+++ handlers/Ahh.py
@@ -14,17 +14,17 @@ async def hello_world(client: Client, message: Message):
     await asyncio.sleep(0.2)
     await mg.edit("aahh")
     await asyncio.sleep(0.2)
-    await mg.edit("aahhh")
+    await mg.edit(":-(")
     await asyncio.sleep(0.2) 
     await mg.edit("aahhhh")
     await asyncio.sleep(0.2) 
-    await mg.edit("aahhhhh")
+    await mg.edit("(TдT)")
     await asyncio.sleep(0.2) 
     await mg.edit("aahhhhhh") 
     await asyncio.sleep(0.2) 
     await mg.edit("aahhhhhhh") 
     await asyncio.sleep(0.2) 
-    await mg.edit("aaahhhhhhhh")
+    await mg.edit(" (-̩̩̩-̩̩̩-̩̩̩-̩̩̩-̩̩̩___-̩̩̩-̩̩̩-̩̩̩-̩̩̩-̩̩̩)")
 
 
 add_command_help(
